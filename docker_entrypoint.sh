e5be1018efc6093121264de1daaf4c3a39cc1176
Remco Ros <remcoros@live.nl>
2024-07-28 15:37:01 +0000

    fix: built-in node rpc host must have http://

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -7,7 +7,7 @@ echo "Starting NFC Push TX..."
 echo
 
 if [ $(yq e '.advanced.use_custom_node' /home/app/start9/config.yaml) = "false" ]; then
-    export RPC_HOST="bitcoind.embassy:8332"
+    export RPC_HOST="http://bitcoind.embassy:8332"
     export RPC_USERNAME=$(yq e '.bitcoind_rpcusername' /home/app/start9/config.yaml)
     export RPC_PASSWORD=$(yq e '.bitcoind_rpcpassword' /home/app/start9/config.yaml)
     echo "Use built-in Bitcoin Core node: $RPC_USERNAME@$RPC_HOST"

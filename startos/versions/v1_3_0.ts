bc5fbdcf6273fb97d55f97d24487551111d7af96
Remco Ros <remcoros@live.nl>
2026-04-15 15:01:01 +0200

    chore: beta version

--- startos/versions/v1_3_0.ts
+++ startos/versions/v1_3_0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v1_3_0 = VersionInfo.of({
-  version: '1.3.0:1.0',
+  version: '1.3.0:1-beta.1',
   releaseNotes: { en_US: 'Revamped for StartOS 0.4.0' },
   migrations: {
     up: async ({ effects }) => {},

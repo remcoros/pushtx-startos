d3500b660fb599c9ddace7f01f5eb441af9d4fbc
Remco Ros <remcoros@live.nl>
2026-05-14 18:52:36 +0200

    chore: release 1.3.0:1

--- startos/versions/v1_3_0.ts
+++ startos/versions/v1_3_0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v1_3_0 = VersionInfo.of({
-  version: '1.3.0:1-beta.2',
+  version: '1.3.0:1',
   releaseNotes: {
     en_US: 'Revamped for StartOS 0.4.0',
     es_ES: 'Renovado para StartOS 0.4.0',
